b364a1e3593e0245e02c1436e3791b0dc751f6c8
DheoAp <dheoap21@gmail.com>
2020-07-11 10:50:18 +0700

    menbuat fitur auth

--- latihan_login/accounts/views.py
+++ latihan_login/accounts/views.py
@@ -1,19 +1,22 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages # untuk pesan error atau lainnya
 # Create your views here.
+from .decorators import unauthenticated_user
 
+@unauthenticated_user
+def index(request):
+    return render (request, 'user/index.html')
 
-class Index(TemplateView):
-    template_name = 'user/index.html'
+@login_required(login_url='login')
+def pageAdmin(request):
+    return render(request,'user/admin.html')
 
-
-class PageAdmin(LoginRequiredMixin,TemplateView):
-    template_name = 'user/admin.html'
-
-
+@unauthenticated_user # jika sudah masuk ke halaman admin makan tidak bisa masuk page login, harus logout dulu
 def LoginPage(request):
 
     if request.method == 'POST':
@@ -26,7 +29,7 @@ def LoginPage(request):
             login(request, user)
             return redirect('page-admin')
         else:
-            message.info(request,'Username or Password is incorrect')
+            messages.info(request,'Username or Password is incorrect')
 
     context = {}
     return render(request, 'user/login.html', context)
--- latihan_login/mywebsite/urls.py
+++ latihan_login/mywebsite/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from accounts.views import Index, PageAdmin, LoginPage, LogoutPage
+from accounts.views import index, pageAdmin, LoginPage, LogoutPage
 
 urlpatterns = [
-    path('', Index.as_view(), name='index'),
-    path('PageAdmin',login_required( PageAdmin.as_view()), name='page-admin'),
+    path('', index, name='index'),
+    path('PageAdmin',pageAdmin, name='page-admin'),
 
     path('login',LoginPage, name='login'),
     path('logout',LogoutPage, name='logout'),
